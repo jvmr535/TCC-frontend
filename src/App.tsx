3b52d9c868342ef25e49aa306e017674b3d4ce27
jvmr535 <46688620+jvmr535@users.noreply.github.com>
2021-11-02 20:11:31 -0300

    feat: :sparkles: First commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+
+import GlobalStyles from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyles />
+
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
